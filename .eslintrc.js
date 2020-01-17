60eaedcb2bbf9d0463843ad790e58432311e7606
junjian.xjj <junjian.xjj@alibaba-inc.com>
2020-01-17 10:56:34 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,33 @@
 module.exports = {
-    "extends": ["eslint-config-egg", "standard", "plugin:prettier/recommended"],
-    "env": {
-        "browser": true,
-        "es6": true
+  "extends": ["eslint-config-egg", "standard", "plugin:prettier/recommended"],
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly",
+
+    "ANT_DESIGN_PRO_ONLY_DO_NOT_USE_IN_YOUR_PRODUCTION": true,
+    "page": true,
+  },
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
     },
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "prettier"
-    ],
-    "rules": {
-        "prettier/prettier": "error",
-        "indent": ["error", 2],
-        "linebreak-style": ["error", "windows"],
-        "quotes": [1, "single"],
-        "semi": ["error", "always"],
-        "no-unused-vars": [0, 1]
-    }
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react",
+    "prettier"
+  ],
+  "rules": {
+    "prettier/prettier": "error",
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "windows"],
+    "quotes": [1, "single"],
+    "semi": ["error", "always"],
+    "no-unused-vars": [0, 1]
+  }
 };
